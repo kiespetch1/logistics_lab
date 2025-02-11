4a2543846120e5e8083461a5ab9079abd85032d6
kiespetch <kiespetch@ya.ru>
2025-02-12 00:19:14 +0300

    fix: попытка пофиксить ошибку при билде

--- src/app/api/rss/external/[id]/route.ts
+++ src/app/api/rss/external/[id]/route.ts
@@ -1,18 +1,23 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import prisma from '~/lib/prisma';
 
 export async function DELETE(
-    request: Request,
-    { params }: { params: { id: string } }
+    request: NextRequest,
+    context: Promise<{ params: { id: string | string[] } }>
 ) {
+    const { params } = await context;
+    const id = Array.isArray(params.id) ? params.id[0] : params.id;
+
     try {
-        const id = params.id;
         await prisma.externalFeed.delete({
             where: { id },
         });
         return NextResponse.json({ message: 'Источник удалён' });
     } catch (error) {
         console.error('Ошибка удаления источника:', error);
-        return NextResponse.json({ error: 'Ошибка удаления источника' }, { status: 500 });
+        return NextResponse.json(
+            { error: 'Ошибка удаления источника' },
+            { status: 500 }
+        );
     }
 }
