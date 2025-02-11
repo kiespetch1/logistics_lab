712243f459b07ffc40e3188c3878f321393fa58f
kiespetch <kiespetch@ya.ru>
2025-02-12 00:54:31 +0300

    fix: попытка пофиксить ошибку при билде 9

--- src/app/components/widgets/CurrencyConverter.tsx
+++ src/app/components/widgets/CurrencyConverter.tsx
@@ -1,41 +1,41 @@
-import { useEffect, useRef } from 'react';
+// @ts-nocheck
+import { useEffect, useRef } from 'react'
 
 const CurrencyConverter = () => {
-    const widgetRef = useRef(null);
+    const widgetRef = useRef(null )
 
     useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://s.fx-w.io/widgets/currency-converter/latest.js';
-        script.async = true;
-        document.body.appendChild(script);
+        const script = document.createElement('script')
+        script.src = 'https://s.fx-w.io/widgets/currency-converter/latest.js'
+        script.async = true
+
+        document.body.appendChild(script)
 
         return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
+            document.body.removeChild(script)
+        }
+    }, [])
 
     return (
         <div>
             <div ref={widgetRef}>
-                {(
-                    <fxwidget-cc
-                        amount="1"
-                        decimals="2"
-                        large="false"
-                        shadow="true"
-                        symbol="true"
-                        grouping="true"
-                        border="true"
-                        from="USD"
-                        to="RUB"
-                        backgroundColor="#f0f8ff"
-                        borderRadius="0.1"
-                    ></fxwidget-cc>
-                ) as any}
+                <fxwidget-cc
+                    amount="1"
+                    decimals="2"
+                    large="false"
+                    shadow="true"
+                    symbol="true"
+                    grouping="true"
+                    border="true"
+                    from="USD"
+                    to="RUB"
+                    background-color="#f0f8ff"
+                    border-radius="0.1"
+                ></fxwidget-cc>
             </div>
             <a href="https://currencyrate.today/">CurrencyRate</a>
         </div>
-    );
-};
+    )
+}
 
-export default CurrencyConverter;
+export default CurrencyConverter
