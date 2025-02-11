05507e0a75f4365c5b93b98cf0e34b87c5bf59cf
kiespetch <kiespetch@ya.ru>
2025-02-12 00:22:39 +0300

    fix: попытка пофиксить ошибку при билде 2

--- src/app/api/rss/external/[id]/route.ts
+++ src/app/api/rss/external/[id]/route.ts
@@ -3,9 +3,8 @@ import prisma from '~/lib/prisma';
 
 export async function DELETE(
     request: NextRequest,
-    context: Promise<{ params: { id: string | string[] } }>
+    { params }: { params: { id: string | string[] } }
 ) {
-    const { params } = await context;
     const id = Array.isArray(params.id) ? params.id[0] : params.id;
 
     try {
